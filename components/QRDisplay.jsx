cf0400810adb6bcf99ea882c9bfec7369df0b66a
Martin Laguna <M.Laguna@ibm.com>
2022-04-24 10:45:45 -0300

    QRDisplay implementation on test

--- components/QRDisplay.jsx
+++ components/QRDisplay.jsx
@@ -2,6 +2,8 @@
 // import { Text } from 'react-native'; 
 import styled from 'styled-components/native';
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+// import { generateQR } from '../utils/QRGenerator';
+import QRCode from 'react-native-qrcode-svg';
 
 const Container = styled.View`
     position: absolute;
@@ -20,7 +22,11 @@ const Container = styled.View`
 const QRDisplay = () =>{
     return (
         <Container>
-            <MaterialIcon name="qr-code-2" size={300} />
+            {/* <MaterialIcon name="qr-code-2" size={300} /> */}
+            <QRCode
+                value="45032905"
+                size={300}
+            />
         </Container>
     )
 }
